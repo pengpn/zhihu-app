914ce442311914ec1728d05ef596c713264501bc
pengpn <312924860@qq.com>
2017-09-13 20:52:56 +0800

    add auth middileware

--- app/Http/Controllers/QuestionsContoller.php
+++ app/Http/Controllers/QuestionsContoller.php
@@ -9,6 +9,11 @@ use Auth;
 
 class QuestionsContoller extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index','show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
