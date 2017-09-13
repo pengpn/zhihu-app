75072bc053907b6c4dd96b9dd6280e864e988462
pengpn <312924860@qq.com>
2017-09-13 21:11:54 +0800

    add topic and question relationship

--- app/Question.php
+++ app/Question.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     protected $fillable = ['title','body','user_id'];
+
+    public function Topics()
+    {
+        return $this->belongsToMany(Topic::class)->withTimestamps();
+    }
 }
