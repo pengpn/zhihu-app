876ec7b1e7507683f5ce20052926f04318c4d5c1
pengpn <312924860@qq.com>
2017-09-28 21:49:35 +0800

    finish message notification

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::post('/questions/{question}/answer','AnswersController@store')->name('ans
 Route::get('/questions/{question}/follow','QuestionFollowController@follow')->name('questions.follow');
 
 Route::get('notifications','NotificationsController@index');
+Route::get('notifications/{notification}','NotificationsController@show');
 
 Route::get('inbox','InboxController@index');
 Route::get('inbox/{dialogId}','InboxController@show');
